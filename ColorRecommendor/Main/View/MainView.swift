c13e79e6064b06f8ccf620dcb4de576968d63d06
Daniel Phiri <danielphiri@berkeley.edu>
2021-02-20 21:50:11 -0800

    Finished implenting UI.

--- ColorRecommendor/Main/View/MainView.swift
+++ ColorRecommendor/Main/View/MainView.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct MainView: View {
   private let model                         : MainModelable
-  @State private var menuOpacity            = 1.0
+  @State private var menuOpacity            = 0.0
   @State private var searchingDone          = false
   @State private var searchText             = ""
+  @State private var backgroundColor: Color = Color.white
   
   init(_ model: MainModelable) {
     self.model = model
   }
   
   var body: some View {
-    VStack {
+    VStack(spacing: 0) {
       searchBar
         .frame(width: nil, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-        .padding(.init(top: 0, leading: 5, bottom: 0, trailing: 5))
-      Spacer()
-      
+        .padding(.init(top: 0, leading: 10, bottom: -3, trailing: 5))
+        .background(backgroundColor)
+       ZStack {
+         backgroundColor
+         menuFeedItems
+          .background(backgroundColor)
+       }
     }
   }
   
@@ -38,12 +43,14 @@ struct MainView: View {
             searchingDone = false
           } else {
             menuOpacity = 1.0
+            backgroundColor = Color.white
           }
         }
-        .padding(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-        .cornerRadius(3.0)
+        .font(.system(size: 12, weight: .semibold))
         .foregroundColor(.black)
-        .shadow(color: .green, radius: 1.0, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+        .border(Color.gray, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+        .cornerRadius(2.0)
+        .shadow(color: .green, radius: 1.0, x: 0, y: 0)
       }
     )
   }
@@ -70,31 +77,34 @@ extension MainView {
         List {
           ForEach(model.getAllColors(matching: searchText)) { menuItem in
             HStack {
+              Image.init(systemName: "clock")
+                .foregroundColor(.black)
+                .background(Color.purple)
               
-              Image.init("menu_icon")
-                .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.gray)
-                .background(Color.red)
-//                .scaledToFit()
-              
-              ForEach.init(menuItem.machingIndeces) { res in
-                if res.num == 0 {
-                  Text(res.text)
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.gray)
-                    .padding(EdgeInsets.init(top: -1, leading: -1, bottom: -1, trailing: -1))
-                    .background(Color.green)
-                } else {
-                  Text(res.text)
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.black)
-                    .padding(EdgeInsets.init(top: -1, leading: -1, bottom: -1, trailing: -1))
-                    .background(Color.red)
+              HStack(spacing: 0) {
+                ForEach.init(menuItem.machingIndeces) { res in
+                  if res.num == 0 {
+                    Text(res.text)
+                      .font(.system(size: 12, weight: .semibold))
+                      .foregroundColor(.gray)
+                  } else {
+                    Text(res.text)
+                      .font(.system(size: 12, weight: .semibold))
+                      .foregroundColor(.black)
+                  }
                 }
+              }
+              .onTapGesture {
+                backgroundColor = Color(hex: menuItem.hexValue)
+                menuOpacity = 0.0
+                searchText = menuItem.name
+                searchingDone = true
               }
             }
           }
         }
+        .padding(.init(top: 0, leading: 10, bottom: 0, trailing: 5))
+        .shadow(color: .gray, radius: 1.0, x: 0, y: 0)
       )
       .opacity(menuOpacity)
     }
